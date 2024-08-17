9bea1904bae8a0be1f0a2176975a7c2a6431d254
dgclasher <dg@clasher>
2024-08-17 07:04:57 +0530

    update data fields

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# flatmate-ml
-ML module of FlatMate
+# FlatMate-ML
+![test-package](https://github.com/dgclasher/flatmate-ml/actions/workflows/test-package.yml/badge.svg?branch=main)
+
+FlatMate-ML is the machine learning module of the FlatMate project. This module is responsible for recommending compatible roommates based on user profiles using advanced machine learning techniques.
+
+## Features
+
+- **Profile Matching**: Matches user profiles to recommend the most compatible roommates.
+- **Data Processing**: Handles data preprocessing including one-hot encoding for categorical variables and scaling for numerical variables.
+- **Similarity Calculation**: Uses cosine similarity to compute the similarity between user profiles.
--- utils/recommend.py
+++ utils/recommend.py
@@ -4,14 +4,14 @@ import numpy as np
 import pandas as pd
 from app import app
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 app_path = os.path.join(app.root_path, '..', 'datasets', 'user_profiles.csv')
 data = pd.read_csv(app_path)
 
 encoder_path = os.path.join(app.root_path, '..', 'dumps', 'encoder.pkl')
 scaler_path = os.path.join(app.root_path, '..', 'dumps', 'scaler.pkl')
-combined_data_path = os.path.join(app.root_path, '..', 'dumps', 'combined_data.pkl')
+combined_data_path = os.path.join(
+    app.root_path, '..', 'dumps', 'combined_data.pkl')
 
 with open(encoder_path, "rb") as f:
     encoder = pickle.load(f)
@@ -23,49 +23,48 @@ with open(combined_data_path, "rb") as f:
     X = pickle.load(f)
 
 categorical_columns = [
-    'Gender', 'Occupation', 'Education_Level', 'Preferred_City',
-    'Proximity_to_Workplace/College', 'Preferred_Accommodation',
-    'Daily_Schedule', 'Smoking_Habits', 'Drinking_Habits',
-    'Dietary_Preferences', 'Pet_Preferences', 'Introvert/Extrovert',
-    'Cleanliness_Level', 'Social_Habits', 'Hobbies_and_Interests',
-    'Language_Preferences', 'Preferred_Security_Measures',
-    'Past_Experiences_with_Roommates'
+    'gender', 'occupation', 'educationLevel', 'preferredCity',
+    'preferredAccommodation', 'dailySchedule', 'smokingHabits',
+    'drinkingHabits', 'dietaryPreferences', 'petPreferences',
+    'introvertExtrovert', 'cleanlinessLevel', 'socialHabits',
+    'hobbiesAndInterests', 'languagePreferences', 'preferredSecurityMeasures',
+    'pastExperiencesWithRoommates'
 ]
-numerical_columns = ['Age', 'Budget/Price_Range', 'Number_of_Roommates']
+numerical_columns = ['age', 'budgetPriceRange', 'numberOfRoommates']
 
 def recommend_profiles(user_data, num_recommendations=3):
     new_user_data = pd.DataFrame({
-        'Gender': [user_data['Gender']],
-        'Occupation': [user_data['Occupation']],
-        'Education_Level': [user_data['Education_Level']],
-        'Preferred_City': [user_data['Preferred_City']],
-        'Proximity_to_Workplace/College': [user_data['Proximity_to_Workplace/College']],
-        'Preferred_Accommodation': [user_data['Preferred_Accommodation']],
-        'Daily_Schedule': [user_data['Daily_Schedule']],
-        'Smoking_Habits': [user_data['Smoking_Habits']],
-        'Drinking_Habits': [user_data['Drinking_Habits']],
-        'Dietary_Preferences': [user_data['Dietary_Preferences']],
-        'Pet_Preferences': [user_data['Pet_Preferences']],
-        'Introvert/Extrovert': [user_data['Introvert/Extrovert']],
-        'Cleanliness_Level': [user_data['Cleanliness_Level']],
-        'Social_Habits': [user_data['Social_Habits']],
-        'Hobbies_and_Interests': [user_data['Hobbies_and_Interests']],
-        'Language_Preferences': [user_data['Language_Preferences']],
-        'Preferred_Security_Measures': [user_data['Preferred_Security_Measures']],
-        'Past_Experiences_with_Roommates': [user_data['Past_Experiences_with_Roommates']],
-        'Age': [user_data['Age']],
-        'Budget/Price_Range': [user_data['Budget/Price_Range']],
-        'Number_of_Roommates': [user_data['Number_of_Roommates']]
+        'gender': [user_data['gender']],
+        'occupation': [user_data['occupation']],
+        'educationLevel': [user_data['educationLevel']],
+        'preferredCity': [user_data['preferredCity']],
+        'preferredAccommodation': [user_data['preferredAccommodation']],
+        'dailySchedule': [user_data['dailySchedule']],
+        'smokingHabits': [user_data['smokingHabits']],
+        'drinkingHabits': [user_data['drinkingHabits']],
+        'dietaryPreferences': [user_data['dietaryPreferences']],
+        'petPreferences': [user_data['petPreferences']],
+        'introvertExtrovert': [user_data['introvertExtrovert']],
+        'cleanlinessLevel': [user_data['cleanlinessLevel']],
+        'socialHabits': [user_data['socialHabits']],
+        'hobbiesAndInterests': [user_data['hobbiesAndInterests']],
+        'languagePreferences': [user_data['languagePreferences']],
+        'preferredSecurityMeasures': [user_data['preferredSecurityMeasures']],
+        'pastExperiencesWithRoommates': [user_data['pastExperiencesWithRoommates']],
+        'age': [user_data['age']],
+        'budgetPriceRange': [user_data['budgetPriceRange']],
+        'numberOfRoommates': [user_data['numberOfRoommates']]
     })
 
-    new_user_cats = encoder.transform(new_user_data[categorical_columns]).toarray()
+    new_user_cats = encoder.transform(
+        new_user_data[categorical_columns]).toarray()
     new_user_nums = scaler.transform(new_user_data[numerical_columns])
     new_user_X = np.hstack([new_user_cats, new_user_nums])
 
     new_user_similarities = cosine_similarity(new_user_X, X).flatten()
-    similar_user_indices = np.argsort(new_user_similarities)[::-1][:num_recommendations]
+    similar_user_indices = np.argsort(new_user_similarities)[
+        ::-1][:num_recommendations]
 
     user_ids = data.iloc[similar_user_indices]
-    user_ids = user_ids['User_ID'].values
-    user_ids = user_ids.tolist()
+    user_ids = user_ids['googleId'].values.tolist()
     return user_ids
